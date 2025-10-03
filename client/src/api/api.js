ea47ff6eaccbf96cf58bbc7b6756543a85e6e99f
Abhishek_Borana <141744273+Abhi22shek@users.noreply.github.com>
2025-10-04 04:10:14 +0530

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: "https://employee-data-management-assesement.onrender.com", 
 });
 
 export const getEmployees = () => API.get("/"); 
